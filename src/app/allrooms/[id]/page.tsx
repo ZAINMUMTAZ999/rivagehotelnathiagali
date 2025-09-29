d6ead9f6a3e29a31d1db2fbceed3a2bc8b629ad3
ZAINMUMTAZ999 <mzainmumtaz99@gmail.com>
2025-09-30 03:52:28 +0500

    sssSSSfweaasaa

--- src/app/allrooms/[id]/page.tsx
+++ src/app/allrooms/[id]/page.tsx
@@ -328,7 +328,7 @@ const MyHotelsById = () =>
               {/* <div className=" flex  items-center  justify-center "> */}
         
        <Link href={whatsappUrl} target="_blank" rel="noopener noreferrer">
-                <Button className="flex justify-center items-center w-full   hover:cursor-pointer bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-lg transition-colors duration-200 shadow-sm hover:shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+                <Button className="flex justify-center items-center w-full cursor-pointer  hover:cursor-pointer bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-lg transition-colors duration-200 shadow-sm hover:shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
         Book Now Pay Later
                 </Button>
       </Link>
