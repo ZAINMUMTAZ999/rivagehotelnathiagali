a6693dc8b8b26f8d1af80c4c3c4c9a22a581d19c
ZAINMUMTAZ999 <mzainmumtaz99@gmail.com>
2025-09-29 17:07:42 +0500

    sssms

--- src/app/components/GetEachHotelReviewId.tsx
+++ src/app/components/GetEachHotelReviewId.tsx
@@ -124,13 +124,13 @@ console.log("HotelReviewById",reviewsData);
  
         left-2 top-1/2 -translate-y-1/2 z-10 mt-12 -ml-4 bg-white hover:bg-gray-50 shadow-lg border border-gray-200 w-10 h-10 rounded-full items-center justify-center transition-all duration-200"
       >
-        <span className="text-gray-600 text-lg ">←</span>
+        {/* <span className="text-gray-600 text-lg ">←</span> */}
       </button>
       <button
         onClick={() => scroll("right")}
         className="hidden sm:flex absolute right-2 top-1/2 mt-12  -ml-4 -translate-y-1/2 z-10 bg-white hover:bg-gray-50 shadow-lg border border-gray-200 w-10 h-10 rounded-full items-center justify-center  transition-all duration-200"
       >
-        <span className="text-gray-600 text-lg ">→</span>
+        {/* <span className="text-gray-600 text-lg ">→</span> */}
       </button>
 
       {/* Reviews list */}
