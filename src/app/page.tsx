9ad972289dcb6b5d00c46c40ce60294d2f508e00
ZAINMUMTAZ999 <mzainmumtaz99@gmail.com>
2025-10-03 22:39:16 +0500

    sdsjf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,14 +26,13 @@ export default function Home() {
       <section className="bg-white py-12 md:py-16 border-t border-gray-200">
         <div className="container mx-auto px-4 max-w-7xl" >
           <h2 className="text-3xl md:text-4xl font-bold text-center text-indigo-800 mb-8">
-            Our Location in Nathia Gali
+            Our Location 
           </h2>
-       
-
+  
           <div className="w-full max-w-4xl mx-auto">
             <div className="relative w-full h-96 md:h-[450px] rounded-lg overflow-hidden shadow-lg">
               <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4066.4109750194434!2d73.37977728841653!3d34.07551884727791!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38dfd5762d7b4765%3A0xcb0a4f3dd7101a87!2sCottage%20Nathiagali!5e1!3m2!1sen!2s!4v1758538761264!5m2!1sen!2s"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d16331.505839226038!2d73.07451985!3d33.73100910000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38dfbf7769d89d8d%3A0x5d6d89877331b187!2sF-6%2C%20Islamabad!5e1!3m2!1sen!2s!4v1759513124803!5m2!1sen!2s"
 
                 className="absolute top-0 left-0 w-full h-full border-0 touch-auto"
 
