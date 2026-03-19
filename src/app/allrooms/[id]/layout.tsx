5a389262f78cac84c9922f3c6027c4807907ef93
mkhjy8iy99-byte <mzainmumtaz99@gmail.com>
2026-03-19 13:20:37 +0500

    fix OG meta

---
+++ src/app/allrooms/[id]/layout.tsx
@@ -0,0 +1,49 @@
+
+
+// app/allrooms/[id]/layout.tsx
+
+import { Metadata } from "next"
+const Base_Url_API = "https://deft-choux-8f7a7b.netlify.app/api";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string }
+}): Promise<Metadata> {
+  try {
+    const res = await fetch(`${Base_Url_API}/v2/rooms/${params.id}`, {
+      cache: "no-store",
+    })
+
+    if (!res.ok) throw new Error()
+
+    const hotel = await res.json()
+    const firstImage = hotel?.imageUrls?.[0] ?? ""
+
+    return {
+      title: hotel.name,
+      description: hotel.description,
+      openGraph: {
+        title: hotel.name,
+        description: hotel.description,
+        images: firstImage
+          ? [{ url: firstImage, width: 1200, height: 627 }]
+          : [],
+      },
+    }
+  } catch {
+    return {
+      title: "Hotel",
+      description: "View hotel details",
+    }
+  }
+}
+
+export default function HotelLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <>{children}</>
+}
+
