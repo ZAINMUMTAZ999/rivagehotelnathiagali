d1528dc710cab6f3941d05fb21e2f660e1ded221
Muhammad Zain Mumtaz <zain123@gmail.com>
2026-05-11 14:50:27 -0700

    sdaaa

--- src/app/components/AddHotel.tsx
+++ src/app/components/AddHotel.tsx
@@ -235,7 +235,7 @@ const roomStatusOptions = ["Available","Booked","Maintenance"];
           </h1>
           <Link
             href="/dashboard"
-            className="inline-flex items-center rounded-xl bg-gradient-to-r from-blue-700 to-rose-600 px-4 py-2 text-white font-semibold shadow hover:from-red-700 hover:to-rose-700 focus:outline-none focus:ring-2 focus:ring-rose-400 focus:ring-offset-2 transition-all text-sm" 
+            className="inline-flex items-center rounded-xl bg-blue-600  px-4 py-2 text-white font-semibold shadow hover:from-red-700 hover:to-rose-700 focus:outline-none focus:ring-2 focus:ring-rose-400 focus:ring-offset-2 transition-all text-sm" 
           >
             Go to Dashboard 
           </Link>
