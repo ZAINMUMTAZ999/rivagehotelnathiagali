f5ddbd7840f9fe569d520a56cbdac4892476d96c
M Zain Mumtaz <mzainmumtaz99@gmail.com>
2026-08-05 12:43:43 +0500

    UI U

--- src/app/allrooms/[id]/page.tsx
+++ src/app/allrooms/[id]/page.tsx
@@ -247,7 +247,8 @@ queryKey: ["hotel", id],
   };
 
   return (
-    <div className="mt-8 w-3/4">
+    // <div className="mt-8 w-3/4">
+    <div className="mx-auto mt-8 w-full max-w-7xl px-4 lg:px-8">
       {/* Header */}
       <div className="bg-white/70 backdrop-blur-md border-b border-slate-200/40 sticky top-0 z-10">
         <div className="flex items-center justify-between px-4 py-4">
