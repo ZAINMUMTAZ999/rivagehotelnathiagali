c5dba96a7b23c93fe2acb73f492dc59e7c8d0872
M Zain Mumtaz <mzainmumtaz99@gmail.com>
2026-09-21 23:01:55 +0500

    fix css responsiveness of homepage

--- src/app/components/HotelServices.tsx
+++ src/app/components/HotelServices.tsx
@@ -69,7 +69,7 @@ const HotelServicesSection: React.FC = () => {
 
       <div className="max-w-7xl mx-auto px-6 md:px-8 text-center mb-16">
         <motion.h2
-          className="text-base sm:text-lg  md:text-3xl lg:text-5xl xl:text-6xl font-extrabold text-indigo-800 mb-4"
+          className="text-xl  md:text-3xl lg:text-5xl xl:text-6xl font-extrabold text-indigo-800 mb-4"
           variants={itemVariants} // Animate the title
         >
           Our Signature Hotel Services
@@ -82,19 +82,19 @@ const HotelServicesSection: React.FC = () => {
         </motion.p>
       </div>
 
-      <div className="max-w-7xl mx-auto px-6 md:px-8">
+      <div className="max-w-7xl mx-auto px-4 md:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-8 md:gap-10">
           {services.map((service) => (
             <motion.div
               key={service.id}
-              className="bg-gray-50 rounded-xl shadow-md hover:shadow-xl transition-shadow duration-300 p-4 sm:p-6 md:p-8 flex flex-col items-center text-center border-t-4 border-emerald-500"
+              className="bg-gray-50 rounded-xl shadow-md hover:shadow-xl transition-shadow duration-300 p-4 sm:p-6 md:p-8 flex flex-col items-center text-center border-t-4 border-blue-500"
               variants={itemVariants} // Animate each card
             >
               <div className="mb-6 bg-indigo-100 text-indigo-700 p-2 md:p-4 rounded-full inline-flex items-center justify-center shadow-inner">
                 <service.icon size={40} className="stroke-2" /> {/* Render Lucide icon */}
               </div>
               <h3 className="
-              text-base
+              text-sm
               md:text-xl lg:text-2xl font-semibold text-gray-900 mb-3">
                 {service.title}
               </h3>
